6178a99c9c9b35146966867f2901b136952cc6cd
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-18 19:28:32 +0530

    c

---
+++ array.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int marks [10];
+   printf("enter math num \n");
+   scanf("%d",&marks[0]);
+     printf("enter math num \n");
+   scanf("%d",&marks[1]);
+     printf("enter math num \n");
+   scanf("%d",&marks[2]);
+   printf ("%d,%d.%d",marks[0],marks[1],marks[2]);
+    return 0;
+}
---
+++ gstarr.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   float marks [10];
+   
+   printf("enter 1amount \n");
+   scanf("%f",&marks[0]);
+    printf(" with gst %f \n",(0.18*marks[0])+marks[0]);
+   
+   
+   
+   
+   
+
+   
+    return 0;
+}
